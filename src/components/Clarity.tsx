cdc244f1253e62826b4ed5c53bf396efef80c878
Maitrek Patel <maitrekpatel1612@gmail.com>
2026-07-01 01:19:40 +0530

    feat: Add Clarity component for Microsoft Clarity integration

---
+++ src/components/Clarity.tsx
@@ -0,0 +1,18 @@
+// app/components/Clarity.tsx
+"use client";
+
+import Script from "next/script";
+
+export default function Clarity() {
+    return (
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+            {`
+        (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "xfa54tpoja");
+      `}
+        </Script>
+    );
+}
